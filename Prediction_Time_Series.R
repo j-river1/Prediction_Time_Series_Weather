7f175ba75b66c43ea542c718b8d34cfc8ed2956d
Juan Camilo Rivera <j-river1@uniandes.edu.co>
2017-12-05 10:21:10 -0500

    Code for weather

--- Prediction_Time_Series.R
+++ Prediction_Time_Series.R
@@ -2,5 +2,22 @@
 
 #Read files 
 
-files <- lapply(list.files(pattern ="\\.csv$"), function(x) {read.csv(x, header = TRUE)})
-names(files) 
+files <- lapply(list.files(path = "../TrainingData"), function(x) {
+namefile <- x
+name <- paste0("../TrainingData/", x)
+file <- read.csv(name, header = TRUE)
+return (file)})
+
+  
+#Put names to file
+#Argument. -names. File name. eg. tread.csv
+#return    -names. File name tread
+name_files <- function (names)
+{
+  names <- gsub(".csv", "", names)
+  return (names)
+}
+
+#Put the names list files
+names(files) <- name_files(list.files(path = "../TrainingData"))
+
